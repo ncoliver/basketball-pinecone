d0e86ee76270e70dd0ef59c00aab56f446c7b675
ncmoliver <mrmarquesoliver@gmail.com>
2025-05-22 23:26:45 -0400

    data added: pinecone

--- basketballaipc.py
+++ basketballaipc.py
@@ -1,18 +1,17 @@
-from pinecone import Pinecone, ServerlessSpec
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from pinecone import Pinecone, ServerlessSpec
 
 load_dotenv()
 
 api_key = os.getenv("PINECONE_API_KEY")
-
 pc = Pinecone(api_key=api_key)
 
 index_name = "quickstart"
 
 pc.create_index(
     name=index_name,
-    dimension=1024, # Replace with your model dimensions
+    dimension=8, # Replace with your model dimensions
     metric="euclidean", # Replace with your model metric
     spec=ServerlessSpec(
         cloud="aws",
